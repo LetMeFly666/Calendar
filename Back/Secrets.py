4e99ed35bf7cdccd1d11684c790abc18b31a0409
LetMeFly666 <814114971@qq.com>
2022-02-02 12:43:08 +0800

    此行为了使项目中存在Secrets.py文件

---
+++ Back/Secrets.py
@@ -0,0 +1,9 @@
+'''
+Author: LetMeFly
+Date: 2022-02-02 12:36:27
+LastEditors: LetMeFly
+LastEditTime: 2022-02-02 12:41:09
+Description: 
+    这里存放一些密码之类的东西，包括数据库用户名密码等
+    该文件中有很多内容未同步到GitHub中，稍后将会在.gitignore中添加此文件
+'''
