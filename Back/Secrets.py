058bea3d79c18c0d10f7662492a19ef887edb23a
LetMeFly666 <814114971@qq.com>
2022-02-02 19:05:18 +0800

    发现如果.gitignore之前追踪了某个文件，那么ignore后文件的更新照样会被追踪

--- Back/Secrets.py
+++
@@ -1,9 +0,0 @@
-'''
-Author: LetMeFly
-Date: 2022-02-02 12:36:27
-LastEditors: LetMeFly
-LastEditTime: 2022-02-02 15:49:37
-Description: 
-    这里存放一些密码之类的东西，包括数据库用户名密码等
-    该文件中有很多内容未同步到GitHub中，稍后将会在.gitignore中添加此文件
-'''
