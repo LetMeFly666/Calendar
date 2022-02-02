3062228f5918d4c00acdf3c0631124f1ffaa4966
LetMeFly666 <814114971@qq.com>
2022-02-02 19:17:52 +0800

    添加了SVN，并且修改了SECRET_KEY，且移动到了Secrets.py中

--- Back/Diary/settings.py
+++ Back/Diary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .. import Secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j9s@5o(xqm6ie7lh!7u+w)i!y_dlj209e6z5n#c93y6!_g&i-w'
+SECRET_KEY = Secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
