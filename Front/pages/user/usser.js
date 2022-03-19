4ad09646e0578933d4ad1c9d7ad06293b6df50da
LetMeFly666 <814114971@qq.com>
2022-03-19 19:43:53 +0800

    放弃获取用户头像昵称

--- Front/pages/user/usser.js
+++ Front/pages/user/usser.js
@@ -5,15 +5,43 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        // userInfo: {},
+        // hasUserInfo: false,
+        // canIUseGetUserProfile: false,
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        // if (wx.getUserProfile) {
+        //     this.setData({
+        //         canIUseGetUserProfile: true
+        //     })
+        // }
     },
+
+    // getUserProfile(e) {
+    //     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    //     // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    //     wx.getUserProfile({
+    //         desc: "展示你的头像昵称信息", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+    //         success: (res) => {
+    //             this.setData({
+    //                 userInfo: res.userInfo,
+    //                 hasUserInfo: true
+    //             })
+    //         }
+    //     })
+    // },
+
+    // getUserInfo(e) {
+    //     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接回匿  名的用户个人信息
+    //     this.setData({
+    //         userInfo: e.detail.userInfo,
+    //         hasUserInfo: true
+    //     })
+    // },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
